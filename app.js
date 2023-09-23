3cd25ab0b6c67be301d680b012b500818dd0abd0
ramdani221 <ramdani.hidayat221@gmail.com>
2023-09-23 17:39:33 +0700

    API done

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+const fileUpload = require('express-fileupload');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(fileUpload());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/api.js
+++ routes/api.js
@@ -2,29 +2,25 @@ var express = require('express');
 var router = express.Router();
 const models = require('../models')
 const { Op } = require("sequelize");
+const path = require("path")
+const fs = require('fs')
 
 /* GET home page. */
 router.get('/phonebooks', async function (req, res, next) {
-    const { page = 1, limit = 10, keyword = '', sort = 'asc' } = req.query
+    const { page = 1, limit = 10, keyword = '', sort = 'ASC' } = req.query
+
     try {
-        if (Number.isInteger(Number(keyword))) {
-            const { count, rows } = await models.Phonebooks.findAndCountAll({
-                where: {
-                    phone: {[Op.like]: `%${keyword}%`}
-                }
-            })
-            const pages = Math.ceil(count / limit)
-            res.status(200).json({ phonebooks: rows, page, limit, pages, total: count })
-        } else {
-            const { count, rows } = await models.Phonebooks.findAndCountAll({
-                where: {
-                    name: {[Op.iLike]: `%${keyword}%`}
-                },
-                order: [['name', sort]]
-            })
-            const pages = Math.ceil(count / limit)
-            res.status(200).json({ phonebooks: rows, page, limit, pages, total: count })
-        }
+        const { count, rows } = await models.Phonebooks.findAndCountAll({
+            where: {
+                [Op.or]: [
+                    { name: { [Op.iLike]: `%${keyword}%` } },
+                    { phone: { [Op.like]: `%${keyword}%` } }
+                ]
+            },
+            order: [['name', sort]]
+        })
+        const pages = Math.ceil(count / limit)
+        res.status(200).json({ phonebooks: rows, page, limit, pages, total: count })
     } catch (err) {
         console.log(err)
         res.status(500).json(err)
@@ -78,19 +74,50 @@ router.delete('/phonebooks/:id', async function (req, res, next) {
 });
 
 router.put('/phonebooks/:id/avatar', async function (req, res, next) {
-    const { avatar } = req.body
-    try {
-        const phonebooks = await models.Phonebooks.update({ avatar }, {
-            where: {
-                id: req.params.id
-            },
-            returning: true,
-            plain: true
-        });
-        res.status(201).json(phonebooks[1])
-    } catch (err) {
-        res.status(500).json(err)
+    let avatar;
+    let uploadPath;
+
+    console.log(req.files.avatar)
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
     }
+    
+    avatar = req.files.avatar;
+    let fileName = Date.now() + '_' + avatar.name
+    uploadPath = path.join(__dirname, '..', 'public', 'images', fileName);
+
+    avatar.mv(uploadPath, async function (err) {
+        if (err)
+            return res.status(500).send(err);
+        try {
+            const profile = await models.Phonebooks.findOne({ where: { id: req.params.id } });
+            if (profile.avatar) {
+                const filePath = path.join(__dirname, '..', 'public', 'images', profile.avatar);
+                try { fs.unlinkSync(filePath) } catch {
+                    const phonebooks = await models.Phonebooks.update({ avatar: fileName }, {
+                        where: {
+                            id: req.params.id
+                        },
+                        returning: true,
+                        plain: true
+                    });
+                    return res.status(201).json(phonebooks[1])
+                }
+            }
+            const phonebooks = await models.Phonebooks.update({ avatar: fileName }, {
+                where: {
+                    id: req.params.id
+                },
+                returning: true,
+                plain: true
+            });
+            res.status(201).json(phonebooks[1])
+        } catch (err) {
+            console.log(err)
+            res.status(500).json(err)
+        }
+    });
 });
 
 module.exports = router;
